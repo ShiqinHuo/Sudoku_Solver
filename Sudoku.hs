da2a521992c0517724cfce51370528d7cdadb6f5
Shiqin Huo <u5949730@anu.edu.au>
2017-05-27 00:43:56 +1000

    add Quickcheck for solve

--- Sudoku.hs
+++ Sudoku.hs
@@ -183,22 +183,18 @@ instance Arbitrary Sudoku where
     return (Sudoku rows)
 -- fromString converts an 81-character canonical string encoding for a
 -- Sudoku into our internal representation
-
 fromString :: String -> Sudoku
 fromString xs = Sudoku (group xs)
-
 -- group a string into a nested list consisting of 9 lists
 group :: String -> Matrix Cell
 group xs = chunk 9 (concat [convert s | s <- ys])
     where
         ys = lines xs
-
 chunk :: Int -> Row Cell -> Matrix Cell
 chunk _ [] = []
 chunk n l
     | n > 0 = (take n l) : (chunk n (drop n l))
     | otherwise = error "Negative n"
-
 -- converts a string into a row
 convert :: String -> Row Cell
 convert str = case str of
@@ -210,7 +206,6 @@ convert str = case str of
 -- toString converts a Sudoku into its canonical 81-character string
 -- encoding
 -- prop> fromString (toString s) == s
-
 toString :: Sudoku -> String
 toString (Sudoku s) = concat $ [printRow (selectRow s x) | x <- [0..8]] -- traversal of all rows (x is the index)
     where
@@ -218,32 +213,26 @@ toString (Sudoku s) = concat $ [printRow (selectRow s x) | x <- [0..8]] -- trave
             []           -> ""
             (Nothing:xs) -> "." ++ printRow xs
             (Just a:xs)  -> [intToDigit a] ++ printRow xs
-
 -- the Int represents the row index in the matrix
 selectRow :: Matrix Cell -> Int -> Row Cell
 selectRow (x:_) 0  = x
 selectRow (x:xs) n = selectRow xs (n-1)
 type Block a = [a]
-
 -- | rows cols boxs are blocks which have to satisfy 2 constraints : No blanks & No duplicates when finished
 rows :: Matrix a -> [Block a]
 rows = id
-
 cols :: Matrix a -> [Block a]
 cols = transpose
-
 boxs :: Matrix a -> [Block a]
 boxs = unpack . (map cols) . pack
     where
         pack = split . map split
         split = chop 3
         unpack = map concat . concat
-
 -- Split the list in sublists of length N (the given int)
 chop :: Int -> [a] -> [[a]]
 chop n [] = []
 chop n xs = take n xs : chop n (drop n xs)
-
 -- Test if a block of cells does not contain the same integer twice
 -- >>> okBlock [Just 1, Just 7, Nothing, Nothing, Just 3, Nothing, Nothing, Nothing, Just 2]
 -- True
@@ -251,14 +240,12 @@ chop n xs = take n xs : chop n (drop n xs)
 -- False
 okBlock :: Block Cell -> Bool
 okBlock b = nub (removeNothing b) == removeNothing b
-
 -- Remove 'Nothing'
 removeNothing :: Block Cell -> Block Cell
 removeNothing b = case b of
     []           -> []
     (Nothing:xs) -> removeNothing xs
     (x:xs)       -> [x] ++ removeNothing xs
-
 -- No block contains the same integer twice
 -- >>> okSudoku allBlanks
 -- True
@@ -273,9 +260,7 @@ okSudoku (Sudoku s) = all okBlock (boxs s) &&
                       all okBlock (cols s)
 -- okSudoku constrains the duplicates
 -- noBlanks constrains the sudoku is finished without "Nothing" position
-
 type Pos = (Int, Int)
-
 -- Return a blank position in the Sudoku
 -- >>> blank allBlanks
 -- (0,0)
@@ -283,37 +268,31 @@ type Pos = (Int, Int)
 -- (0,2)
 blank :: Sudoku -> Pos
 blank = head . allBlankPos
-
 -- Finds all blank spaces in a sudoku
 allBlankPos :: Sudoku -> [Pos]
 allBlankPos = toPosList . getBlanks . indexed
     where getBlanksRow = map fst . filter (isNothing . snd)
           getBlanks    = map (fmap getBlanksRow)
           toPosList    = concatMap (\(r, xs) -> map (r,) xs)
-
 indexed :: Sudoku -> [(Int, [(Int, Maybe Int)])]
 indexed = zip [0..8] . map (zip [0..8]) . cells
-
 -- Given a list, and a tuple containing an index in the list and a new value,
 -- update the given list with the new value at the given index.
 -- >>> ["a","b","c","d"] !!= (1,"apa")
 -- ["a","apa","c","d"]
 -- >>> ["p","qq","rrr"] !!= (0,"bepa")
 -- ["bepa","qq","rrr"]
-
 -- The replace operator
 (!!=) :: [a] -> (Int, a) -> [a]
 (!!=) l (i, x)
     | i < 0 || i > (length l)-1 = l
     | otherwise                 = (take i l) ++ [x] ++ (drop (i+1) l)
-
 -- Given a Sudoku, a position, and a new cell value,
 -- update the given Sudoku at the given position with the new value.
 update :: Sudoku -> Pos -> Int -> Sudoku
 update (Sudoku s) (r, c) i = Sudoku (take r s ++ [(s!!r) !!= (c, toCell i)] ++ drop (r+1) s)
     where
         toCell x = Just x
-
 -- Solver - LEVEL 0 : Brute Force / Backtracking
 -- solve takes an 81-character encoding of a Sudoku puzzle and returns a
 -- list of solutions for it, if any
@@ -330,7 +309,6 @@ solve_level_0 str
             str_list = [toString (update s nextBlank i) | i <- [1..9]]
 -- Just leave this very slow solver here
 -- If you'd like to test this slow solution, simply change the "solve" to "solve_level_0" in Main.hs
-
 ---------- Above is based on Tony's skeleton.
 -- QuickCheck:
 prop_allBlank :: Bool
@@ -339,21 +317,17 @@ prop_allBlank = let m = cells allBlanks
                  length m == 9
                  && and (map ((==9) . length) m)
                  && and ((concatMap (map isNothing)) m)
-
 -- whether the sudoku is valid
 prop_Sudoku :: Sudoku -> Bool
 prop_Sudoku (Sudoku s) = isSudoku (Sudoku s)
-
 -- whether the found position is really a blank
 prop_blank :: Sudoku -> Bool
 prop_blank s = let m        = cells s
                    (x,y)    = blank s
                in isNothing ((m !! x) !! y)
-
 -- (!!=): After replacing an element, the length should be the same
 prop_replace :: [a] -> (Int, a) -> Bool
 prop_replace s (i,x) = length s == length (s !!= (i, x))
-
 -- checks the updated position
 prop_update :: Sudoku -> Pos -> Int -> Bool
 prop_update s p i = let (r,c) = p
@@ -366,10 +340,6 @@ toMaybe :: Int -> Maybe Int
 toMaybe 0 = Nothing
 toMaybe i = Just i
 
-fewerCheck prop = quickCheckWith (stdArgs{ maxSuccess = 30 })  prop
-
-
-
 -- Extensions begin here:
 -- 1. Optimize guessing order
 -- 2. Apply Constraint Propagation (does not use Tony's type signature for Propagate):
@@ -381,26 +351,21 @@ values :: [Value]
 values = [Just i | i <- [1..9] ]
 empty :: Value -> Bool
 empty = (== Nothing)
-
 -- Assigns all the possible choices to a Sudoku Puzzle
 choices :: Matrix Value -> Matrix Choices
 choices = map (map choice)
     where
         choice v = if empty v then values else [v]
-
 -- Reduce a matrix of choices to a choice of possible matrices
 collapse :: Matrix [a] -> [Matrix a]
-
 -- A matrix of choices is represented as a choice of Matrix as the Cartesian product of a list of lists
 -- [[1,2],[3,4],[5,6]] -> [[1,3,5],[1,3,6],[1,4,6],[2,3,5],[2,3,6],[2,4,5],[2,4,6]]
 -- helper function :
 cp :: [[a]] -> [[a]]
 cp [] = [[]]
 cp (x:xs) = [y:ys | y <- x, ys <- cp xs]
-
 -- Implement collapse
 collapse = cp . map cp
-
 -- Solving: main purpose is to convert between types: Sudoku, Matrix and String.
 -- related to the use of list comprehension & nested list
 solve :: String -> [String]
@@ -409,7 +374,6 @@ solve str = to_listOfStr list_Of_matrix
         sud = fromString str
         list_Of_matrix = fromSudoku sud
         to_listOfStr list_Of_matrix = [ toString' m | m <- list_Of_matrix ]
-
 -- converts a matrix to string (typical Top-down)
 toString' :: Matrix Cell -> String
 toString' m = concat $ [printRow (selectRow m x) | x <- [0..8]] -- traversal of all rows (x is the index)
@@ -418,49 +382,40 @@ toString' m = concat $ [printRow (selectRow m x) | x <- [0..8]] -- traversal of 
             []           -> ""
             (Nothing:xs) -> "." ++ printRow xs
             (Just a:xs)  -> [intToDigit a] ++ printRow xs
-
 -- converts Sudoku to choices matrix which is applied in pruning part (typical Top-down)
 fromSudoku :: Sudoku -> [Matrix Value]
 fromSudoku (Sudoku m) = search (prune_level_3 (choices m))
                                          -- change the number 1,2,3 to control different levels
                                          --'search' function should also be changed as the same time
-
 -- recursively applies the function f to x as long as x /= f(x)
 -- This term in math is a "fix point":
 fixpoint :: Eq a => (a -> a) -> a -> a
 fixpoint f x = if x == x' then x else fixpoint f x'
     where
         x' = f x
-
 -- searching
 search :: Matrix Choices -> [Matrix Value]
 search m
     | unsolvable m || not (satisfy m) = []
     | complete m = collapse m
-    | otherwise = [s | m' <- expandFirst m, s <- search (fixpoint prune_level_3 m')]
+    | otherwise = [s | m' <- expandFirst m, s <- search (fixpoint prune_level_2 m')]
                                          -- change the number 1,2,3 to control different levels
                                          -- 'fromSudoku' function should also be changed as the same time
                                          -- replace 'expandFirst' by 'expand to test the naive expand
-
 -- Expand WITHOUT considering the guessing order
 expand :: Matrix Choices -> [Matrix Choices]
 expand m =[rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c <- cs]
     where
         (rows1, row : rows2) = break (any(not . is_single)) m
         (row1, cs : row2) = break (not . is_single) row
-
 -- EXTENSION 1: Optimizing the guessing order
-
 -- We are required to find the block with fewest candidates to reduce redundancy
 -- In my implementation, I use num of choice to represent the levels of difficulty to work out
 -- that single position by guessing.
 -- Obviously, if the num of choice equals to min_choice(>1), it will have least redundancy.
-
 -- expandFirst filters the best position to fill first according to the number of possibilities,
 -- which is exactly what we want in the extension 1.
-
 -- |If you want to test expanding without optimization, replace 'expandFirst' in 'search' by 'expand
-
 -- Performs the expansion beginning with the smallest number of choices (>1)
 -- breaks up a matrix is broken into five pieces:
 -- mc = rows1 ++ [row1 ++ cs : row2] ++ rows2
@@ -472,7 +427,6 @@ expandFirst mc  = [rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c <- cs]
         best cs           = (length cs == n)
         n                 = min_choice mc
         min_choice         = minimum . filter(>1) . concat . map(map length)
-
 -- Extension 2 : Constraint Propagation to prune the huge search space
 --  I implement 3 levels :
 --  level_1: prune singles
@@ -482,7 +436,6 @@ expandFirst mc  = [rows1 ++ [row1 ++ [c] : row2] ++ rows2 | c <- cs]
 --  This means, other positions in the blocks related to this known position cannot choose that value, so we have to
 --  reduce this value from the choices list of all related positions (sharing row/col/box)
 --  LEVEL 1 for Constraint Propagation is to "remove singles"
-
 --  Solver - LEVEL 1 : Prune the single choice (a value occurs only once for a block)
 -- minus will remove the second set from the first:
 minus :: Choices -> Choices -> Choices
@@ -492,19 +445,16 @@ reduce_single :: Block Choices -> Block Choices
 reduce_single x = [xs `minus` singles | xs <- x ]
     where
         singles = concat (filter is_single x)
-
 -- remove any duplicate choice in any blocks(rows/cols/boxs) related
 prune_level_1 :: Matrix Choices -> Matrix Choices
 prune_level_1 = pruneBy boxs . pruneBy cols . pruneBy rows
     where
         pruneBy f = f . map reduce_single . f
-
 --  Solver - LEVEL 2 : Prune the naked pairs
 --  In a Block Choices, we find exactly two positions with the choices [2,3].
 --  Then obviously, the value 2 and 3 can only occur in those two places.
 --  All other occurrences of the value 1 and 9 can eliminated
 --  eliminate other occurrences if there exist dup pairs
-
 -- prop> reduce_samepair [[Just 2,Just 3, Just 4, Just 5], [Just 2,Just 3,Just 4],[Just 2,Just 3],[Just 4,Just 3,Just 5,Just 1],[Just 2,Just 3],[Just 2,Just 5]]
 -- [[Just 4,Just 5],[Just 4],[Just 2,Just 3],[Just 4,Just 5,Just 1],[Just 2,Just 3],[Just 5]]
 -- dup_pair = [Just 2, Just 3]
@@ -513,39 +463,32 @@ reduce_samepair x = [if (isInfixOf dup_pair xs)&&(dup_pair/=xs) then xs\\dup_pai
     where -- [if (isInfixOf dup_pair xs)&&(dup_pair/=xs) then xs\\dup_pair else xs | xs <- x]
           -- [if (dup_pair == xs) then xs else xs\\dup_pair | xs <- x]
         dup_pair = to_samepair x
-
 -- similar to prune_level_1
 prune_dup_pair :: Matrix Choices -> Matrix Choices
 prune_dup_pair = pruneBy boxs . pruneBy cols . pruneBy rows
     where
         pruneBy f = f . map reduce_samepair . f
-
 -- combine 2 pruning levels
 prune_level_2 :: Matrix Choices -> Matrix Choices
-prune_level_2 x = prune_dup_pair (prune_level_2 x)
-
+prune_level_2 x = prune_dup_pair (prune_level_1 x)
 -- Solver - LEVEL 3 : Prune the naked triples
 -- Resembles Solver - LEVEL 2
 -- In a Block Choices, we find exactly 3 positions with the choices [2,3,9].
 -- All other simultaneous occurrences of the value [2,3,9] in sharing blocks can eliminated
 -- note: higher levels can also be constructed similarly but there is no sense to filter not frequent occasions
-
 -- eliminate other simultaneous occurrences if there exist 3 same triples
 reduce_sametriple :: Block Choices -> Block Choices
 reduce_sametriple x = [if (isInfixOf dup_triple xs)&&(dup_triple/=xs) then xs\\dup_triple else xs | xs <- x]
     where -- [if (dup_triple/=xs) then xs\\dup_triple else xs | xs <- x] -- more efficient but exist bugs
         dup_triple = to_sametriple x -- [2,3,9]
-
 -- similar to prune_level_1 and prune_dup_pair
 prune_dup_triple :: Matrix Choices -> Matrix Choices
 prune_dup_triple = pruneBy boxs . pruneBy cols . pruneBy rows
     where
         pruneBy f = f . map reduce_sametriple . f
-
 -- combine 3 pruning levels
 prune_level_3 :: Matrix Choices -> Matrix Choices
 prune_level_3 x = prune_dup_triple (prune_dup_pair (prune_level_2 x))
-
 -- helper functions:
 is_single :: [a] -> Bool
 is_single [x] = True
@@ -554,27 +497,22 @@ is_pairs :: [a] -> Bool
 is_pairs l = length l == 2
 is_triple :: [a] -> Bool
 is_triple l = length l == 3
-
 --prop> to_samepair [[2,3,4,5],[2,3,4],[2,3],[4,3,5,1],[5,3],[2,3],[2,5]]
 -- [2,3]
 to_samepair :: Eq a => [[a]] -> [a]
 to_samepair x = concat $ getDups pairs
     where
         pairs =  filter is_pairs x
-
 getDups :: Eq a => [a] -> [a] -- eg. [[2,3],[2,3,4]]
 getDups x = x \\ nub x
-
 --prop> to_sametriple [[2,3,4,5],[2,3,4],[2],[4,3,5,1],[5],[2,3,4],[2,3,4],[2,5,3,4,6]]
 -- [2,3,4]
 to_sametriple :: Eq a => [[a]] -> [a]
 to_sametriple xs = concat $ nub $ filter (\x -> count x xs == 3) triples
     where
         triples = filter is_triple xs
-
 count :: Eq a => [a] -> [[a]] -> Int
 count y ys = length $ filter (==y) ys
-
 -- Enforce consistency property:
 -- When solved, each position in the sudoku matrix has only one choice
 complete :: Matrix Choices -> Bool
@@ -588,14 +526,9 @@ consistent = nodup . concat . filter is_single
 -- Check whether there is no duplicate
 nodup :: Eq a => [a] -> Bool
 nodup l = nub l == l
-
 -- Satisfy sudoku constrains:
 satisfy :: Matrix Choices -> Bool
 satisfy m = all consistent (rows m) && all consistent (cols m) && all consistent (boxs m)
-
-
-
-
 -- | reference:
 -- http://www.programming-algorithms.net/article/42521/Sudoku
 -- http://norvig.com/sudoku.html -- constraint propagation
@@ -605,4 +538,18 @@ satisfy m = all consistent (rows m) && all consistent (cols m) && all consistent
 -- http://mfukar.github.io/2015/12/11/haskell-xvi.html
 -- https://github.com/pkukulak/sudoku/blob/master/src/Sudoku.hs
 -- http://stackoverflow.com/questions/12876384/grouping-a-list-into-lists-of-n-elements-in-haskell
+
+-- Quickcheck for solve
+prop_solve :: String -> Bool
+prop_solve s
+    | solution_list == [] = True
+    | otherwise           = isSolutionOf solution_list s
+    where solution_list = solve s
+
+isSolutionOf :: [String] -> String -> Bool
+solution_list `isSolutionOf` str = solution_list == solve str
+-- all (==True) [ xs  solve str | xs <- solution_list]
+
+-- run with fewerCheck if you do not like to wait
+fewerCheck prop = quickCheckWith stdArgs{ maxSuccess = 5 } prop
 
